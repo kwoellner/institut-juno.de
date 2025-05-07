04ee2420ff5a9c8a462d5c11d681072c83e6cd55
Andreas Gallien <gallien@seleos.de>
2025-05-07 10:30:56 +0200

    feat: add website

--- assets/scripts/custom.js
+++ assets/scripts/custom.js
@@ -1,6 +1,2 @@
 $(document).ready(function() {
-    $('ul.nav li a').bind('click', function(e) {
-        $('html,body').animate({scrollTop: $(this.hash).offset().top});
-        e.preventDefault();
-    });
 });
